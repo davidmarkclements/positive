14c8748ecc51d3db940d589f9ab8936d78435be3
davidmarkclements <huperekchuno@googlemail.com>
2015-12-11 00:16:06 +0000

    bugfix

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ var yn = require('yn')
 module.exports = positive
 
 function positive(question, defaultAnswer) {
-  return yn(prompt(question) || defaultAnswer || true)
+  return yn(
+    prompt(question) || 
+    typeof defaultAnswer === 'boolean' ? defaultAnswer : false
+  )
 }
